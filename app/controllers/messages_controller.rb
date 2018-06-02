13fe6d30391e3ab3a34358fcfcc802aa2abd5fb1
今野勲 <porfavor@mbp.local>
2018-06-02 16:22:11 +0900

    first commit

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,20 @@
+class MessagesController < ApplicationController
+
+# 表示
+  def index
+    @messages = Message.message_list
+    @message = Message.new
+  end
+
+  # 書き込み
+  def create
+    @message = Message.new(params.require(:message).permit(:title, :body))
+    if @message.save
+      redirect_to :action => :index
+    else
+      @messages = Message.message_list
+      render :index
+    end
+  end
+
+end
