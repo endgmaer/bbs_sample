cd5d2ef9bf26b02e308b04fcee58c9f0159be00e
今野勲 <porfavor@mbp.local>
2018-06-03 21:48:29 +0900

    検索窓を追加（機能せず）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,19 @@
 class MessagesController < ApplicationController
 
-# 表示
+
+# 表示 検索
   def index
     @messages = Message.message_list
     @message = Message.new
+    @search = Message.search(params[:q])
+    @topics = @search.result
+    @q = Message.with_keywords(params.dig(:q, :keywords)).ransack(params[:q])
+    @keywords = @q.result
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @messages }
+    end
   end
 
   # 書き込み
@@ -15,6 +25,14 @@ class MessagesController < ApplicationController
       @messages = Message.message_list
       render :index
     end
+  end
+
+  # 削除
+  def destroy
+    @messages = Message.find(params[:board_id])
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to messages_list(@messages)
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,19 @@
 class Message < ActiveRecord::Base
-  validates :title, :presence => true, :length => { :maximum => 10 }
-  validates :body, :presence => true, :length => { :maximum => 100 }
+  validates :title, :presence => true, :length => { :maximum => 20 }
+  # validates :deletepwd, :presence => true, :length => { :minimum => 6 }
+  validates :body, :presence => true, :length => { :maximum => 250 }
 
   def self.message_list
     return Message.all.order(:created_at => :desc)
   end
+
+  scope :with_keywords, -> keywords {
+    if keywords.present?
+      columns = [:description]
+      where(keywords.split(/[[:space:]]/).reject(&:empty?).map {|keyword|
+        columns.map { |a| arel_table[a].matches("%#{keyword}%") }.inject(:or)
+      }.inject(:and))
+    end
+  };
+
 end
