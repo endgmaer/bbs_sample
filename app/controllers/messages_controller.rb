a621d45da988da9e0e2a41403ca9905a1a4dae67
djyama3 <racingteamryota@msn.com>
2018-06-30 03:41:56 +0900

    (fix)createできるように

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
-  
+  before_filter :set_search
   helper_method :sort_column, :sort_direction
 
-# 表示 検索
+  # NOTE: 表示 検索
   def index
     @messages = Message.all
     @messages = Message.message_list
@@ -15,45 +15,52 @@ class MessagesController < ApplicationController
     #@orders = Message.order(params[:sortway])
     @messages = Message.page(params[:page])
     if params[:all].present?
-       @messages = @messages.page(params[:page])
+      @messages = @messages.page(params[:page])
     else
-       @messages = @messages.page(params[:page]).per(5)
+      @messages = @messages.page(params[:page]).per(5)
     end
   end
 
-  before_filter :set_search  
-  def set_search
-    @search = Message.search(params[:q])
-  end
-
-
-  private
-  def sort_direction
-    %w[asc desc].include?(params[:direction])?  params[:direction] : "asc"
-  end
-
-  def sort_column
-      Message.column_names.include?(params[:sort])? params[:sort] : "name"
-  end
-
-  #def search
-    #params[:q] ||= {}
-    #params[:q][:s] = %w(author_id category_name created) # 複数指定は配列を渡す
-    #@q = Message.search params[:q]
-    #@q.result
-  #end
-
-  # 書き込み
+  # NOTE:書き込み
   def create
-    @message = Message.create(params.require(:message)
-      .permit(:title, :body))
+    @message = Message.new(message_params)
+
     if @message.save
-      redirect_to :action => :index
+      redirect_to action: :index
     else
       @messages = Message.message_list
       render :index
     end
   end
+
+  def set_search
+    @search = Message.search(params[:q])
+  end
+
+  private
+
+  def message_params
+    params.require(:message)
+      .permit(
+        :title,
+        :body
+      )
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def sort_column
+    Message.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  #def search
+  #params[:q] ||= {}
+  #params[:q][:s] = %w(author_id category_name created) # 複数指定は配列を渡す
+  #@q = Message.search params[:q]
+  #@q.result
+  #end
 
   # 削除
   def destroy
@@ -62,5 +69,4 @@ class MessagesController < ApplicationController
     redirect_to :action => :index
     #@posting.errors.messages
   end
-
 end
