61b5f6010d43946795f1a266489db54c2b9f382e
djyama3 <racingteamryota@msn.com>
2018-07-22 03:05:28 +0900

    (refact)dropsortは使ってなかったので消す

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -1,8 +1,3 @@
-function dropsort() {
-    var browser = document.sort_form.sort.value;
-    location.href = browser
-}
-
 $(document).on('change', '#sort_link_url', function () {  // フォームの値が変わったときになにかするよ！
     if ($(this).val() != '') {
         window.location.href = $(this).val();
