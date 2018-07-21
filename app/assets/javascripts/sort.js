48f8c20e5a1f3a9dd8e790136454ee1289996f8d
djyama3 <racingteamryota@msn.com>
2018-07-22 03:05:00 +0900

    (feat)新着順が選べなくなるのでblank追加

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -3,7 +3,7 @@ function dropsort() {
     location.href = browser
 }
 
-$(document).on('change', '#sort_order', function () {  // フォームの値が変わったときになにかするよ！
+$(document).on('change', '#sort_link_url', function () {  // フォームの値が変わったときになにかするよ！
     if ($(this).val() != '') {
         window.location.href = $(this).val();
     }
